94b1021bc52087924c4549348e6971fd6d53ba3a
AnshumPal <anshum.pal04@gmail.com>
2025-04-05 20:53:46 +0530

    Update: Converted React code to Next.js

--- converted-project/pages/index.tsx
+++ converted-project/pages/index.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
-      <h1>? Hello from Converted Next.js App!</h1>
-      <p>Your code conversion worked!</p>
-    </div>
-  );
-}
+<p>Manually added test line</p>
