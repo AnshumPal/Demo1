462ce5281d954f0f977e0fd56948713c6e7ebb6a
AnshumPal <anshum.pal04@gmail.com>
2025-04-05 20:03:46 +0530

    Added index.tsx, favicon.ico, and tsconfig.json to complete converted Next.js setup

---
+++ converted-project/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>? Hello from Converted Next.js App!</h1>
+      <p>Your code conversion worked!</p>
+    </div>
+  );
+}
